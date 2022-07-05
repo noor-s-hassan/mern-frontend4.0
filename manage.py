9f1cca5b6b759d5c1a89e5a9608801cf88195eb9
noor-s-hassan <108189131+noor-s-hassan@users.noreply.github.com>
2022-07-04 23:10:35 -0400

    Create manage.py

---
+++ manage.py
@@ -0,0 +1,31 @@
+from flask_script import Manager, Server
+from  flask_migrate import Migrate, MigrateCommand
+
+from app import create_app,db
+
+
+from app.models import User,Post,Like,Comment, Quote
+
+# App instance -> creating app instance
+app = create_app('production')
+
+manager = Manager(app)
+manager.add_command('server',Server)
+
+@manager.command
+def test():
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+@manager.shell
+def make_shell_context():
+    return dict(app=app,db=db,User=User,Like=Like, Comment=Comment, Post=Post,Quote=Quote )
+
+
+migrate = Migrate(app,db)
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
